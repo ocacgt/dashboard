90d4049aed5bb04084ff2efbda07b2eaf5398bc6
Oscar de León <odeleongt@gmail.com>
2018-06-02 10:22:40 -0600

    Fix pdf document creation

--- scripts/create-dashboard.R
+++ scripts/create-dashboard.R
@@ -5,7 +5,7 @@
 # Knit and render report to pdf
 rmarkdown::render(
   input = "content/indicadores.Rmd",
-  output_format = "word_document",
+  output_format = "pdf_document",
   output_file = "reporte.pdf",
   quiet = TRUE,
   clean = TRUE,
